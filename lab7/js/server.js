ceff44eb14304616197ab77ed93962a4b6b5b321
Anastasia <nastasamorgenstern64@gmail.com>
2023-01-07 00:00:35 +0100

    lab7 is done by adding object for adding to server.

---
+++ lab7/js/server.js
@@ -0,0 +1,91 @@
+let object = {
+    canvasForm: {
+        display: 'grid',
+        gridTemplateColumns: '1fr',
+        gridTemplateRows: '50px, 1fr'},
+    cssForm: {
+        display: 'grid',
+        gridTemplateColumns: '1fr',
+        gridTemplateRows: '50px, 1fr'},
+    canvasHeader: {
+        display: 'grid',
+        gridTemplateRows: '1fr',
+        gridTemplateColumns: '3fr 2fr'},
+    cssHeader: {
+        display: 'grid',
+        gridTemplateRows: '1fr',
+        gridTemplateColumns: '3fr 2fr'},
+    startButton: {
+        padding: '10px',
+        background: 'white',
+        color: '#D4D4D4',
+        fontSize: '17px',
+        border: "2px solid #D4D4D4",
+        cursor: 'pointer',
+        margin: 'auto',
+        width: '100px'},
+    stopButton: {
+        padding: '10px',
+        background: 'white',
+        color: '#D4D4D4',
+        fontSize: '17px',
+        border: "2px solid #D4D4D4",
+        cursor: 'pointer',
+        margin: 'auto',
+        width: '100px'},
+    reloadButton: {
+        padding: '10px',
+        background: 'white',
+        color: '#D4D4D4',
+        fontSize: '17px',
+        border: "2px solid #D4D4D4",
+        cursor: 'pointer',
+        margin: 'auto',
+        width: '100px'},
+    closeButton: {
+        padding: '10px',
+        background: 'white',
+        color: '#D4D4D4',
+        fontSize: '17px',
+        border: "2px solid #D4D4D4",
+        cursor: 'pointer',
+        margin: 'auto',
+        width: '100px'},
+    canvasControls: {
+        display: 'grid',
+        gridTemplateColumns: '1fr 1fr',
+        gridTemplateRows: '1fr'},
+    cssControls: {
+        display: 'grid',
+        gridTemplateColumns: '1fr 1fr',
+        gridTemplateRows: '1fr'},
+    canvasMessage: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'},
+    cssMessage: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'},
+    canvasAnim: {
+        borderStyle: 'solid',
+        borderColor: 'green',
+        borderWidth: '5px',
+        backgroundImage: 'url("/img/textureSmall.jpg")',
+        display: 'grid',
+        gridTemplate: '1fr / 1fr'},
+    cssAnim: {
+        borderStyle: 'solid',
+        borderColor: 'green',
+        borderWidth: '5px',
+        backgroundImage: 'url("/img/textureSmall.jpg")',
+        position: 'relative'},
+    canvasAnimation: {
+        background: 'transparent',
+        margin: 'auto'},
+    square: {
+        height: '10px',
+        width: '10px',
+        backgroundColor: '#000080',
+        position: 'absolute'}
+}
